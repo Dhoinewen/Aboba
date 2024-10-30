ce799ce26c956efcfe6be321c72a86030c5faa27
Maxim Sidorenko <maxims@aleannlab.com>
2024-10-31 00:15:45 +0200

    test commit12312

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">Some text</p>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">Some docs text</p>
         </a>
 
         <a
